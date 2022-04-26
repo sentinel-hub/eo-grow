bbea005125857d45703fb26231a3008f1c4f7192
Matej Aleksandrov <matej.aleksandrov@sinergise.com>
2022-04-26 19:41:04 +0200

    Grid switching, part 2 (#43)
    * Area manager grid transformations
    
    * code review changes for PR #43
    
    * modified GridTransformation docstring

--- eogrow/core/area/batch.py
+++ eogrow/core/area/batch.py
@@ -1,14 +1,17 @@
 """
 Area managers for Sentinel Hub batch grids
 """
-from typing import Any, List, Tuple
+import warnings
+from typing import Any, List, Tuple, cast
 
 from geopandas import GeoDataFrame
 from pydantic import Field
 
-from sentinelhub import BatchRequest, BatchRequestStatus, BatchSplitter, BBox, SentinelHubBatch
+from eolearn.core.exceptions import EODeprecationWarning
+from sentinelhub import CRS, BatchRequest, BatchRequestStatus, BatchSplitter, BBox, SentinelHubBatch
 
-from ...utils.general import convert_bbox_coords_to_int
+from ...utils.general import convert_bbox_coords_to_int, reduce_to_coprime
+from ...utils.grid import GridTransformation, create_transformations, get_grid_bbox
 from .base import AreaManager
 
 
@@ -138,6 +141,153 @@ class BatchAreaManager(AreaManager):
 
         return AreaManager._to_dataframe_grid(bbox_list, info_list, info_columns)
 
+    def transform_grid(self, target_area_manager: AreaManager) -> List[GridTransformation]:
+        """Calculates how grid should be transformed into another area manager."""
+        self._check_target_compatibility(target_area_manager)
+        target_area_manager = cast(BatchAreaManager, target_area_manager)
+
+        source_grid = self.get_grid(add_bbox_column=True)
+        source_grid = _fix_split_columns(source_grid)
+        target_grid = self._get_target_grid(target_area_manager, source_grid)
+
+        subsplits_x, subsplits_y = zip(self.subsplit, target_area_manager.subsplit)
+        source_shape, target_shape = zip(reduce_to_coprime(*subsplits_x), reduce_to_coprime(*subsplits_y))
+
+        source_grid = self._add_patch_positions(source_grid, source_shape)
+        target_grid = self._add_patch_positions(target_grid, target_shape)
+
+        return create_transformations(source_grid, target_grid, match_columns=["name", "position_x", "position_y"])
+
+    @staticmethod
+    def _add_patch_positions(grid: List[GeoDataFrame], shape: Tuple[int, int]) -> List[GeoDataFrame]:
+        """Adds columns of values that define how sub-tiles should be spatially divided into groups. Members of each
+        group will have the same values in the newly added columns and will spatially form a regular subgrid of given
+        shape."""
+        for gdf in grid:
+            gdf["position_x"] = gdf.split_x // shape[0]
+            gdf["position_y"] = gdf.split_y // shape[1]
+
+        return grid
+
+    def _check_target_compatibility(self, target_area_manager: AreaManager) -> None:
+        """Checks if source and target managers are compatible to define a transformation between them."""
+        if not isinstance(target_area_manager, BatchAreaManager):
+            raise NotImplementedError(
+                f"Grid transformation is only supported into another {self.__class__.__name__}, but got "
+                f"{target_area_manager.__class__.__name__}"
+            )
+
+        for param in ["tiling_grid_id", "resolution"]:
+            if getattr(self.config, param) != getattr(target_area_manager.config, param):
+                raise NotImplementedError(
+                    "Cannot transform grid because source and target area managers have different values of config "
+                    f"parameter {param}."
+                )
+
+    def _get_target_grid(
+        self, target_area_manager: "BatchAreaManager", source_grid: List[GeoDataFrame]
+    ) -> List[GeoDataFrame]:
+        """First, it tries to obtain a target grid from target area manager. That would work if the grid is already
+        cached or if target area manager is able to generate it. If not, then it creates the target grid from the
+        given source grid."""
+        try:
+            return target_area_manager.get_grid(add_bbox_column=True)
+        except MissingBatchIdError:
+            pass
+
+        basic_grid = self._join_batch_grid(source_grid, self.subsplit, self.absolute_buffer)
+        target_grid = self._split_batch_grid(
+            basic_grid, target_area_manager.subsplit, target_area_manager.absolute_buffer
+        )
+        if target_area_manager.subsplit != (1, 1):
+            target_grid = self._filter_grid_with_area(target_grid)
+
+        # Bounding boxes have to be removed before caching into a Geopackage because they are not serializable.
+        target_grid_without_bbox = [gdf.drop(columns=["BBOX"]) for gdf in target_grid]
+        target_area_manager.cache_grid(target_grid_without_bbox)
+
+        return target_grid
+
+    @staticmethod
+    def _join_batch_grid(
+        grid: List[GeoDataFrame], split: Tuple[int, int], buffer: Tuple[float, float]
+    ) -> List[GeoDataFrame]:
+        """Removes buffers and joins sub-tiles from a batch grid into original tiles."""
+        reversed_buffer = -buffer[0], -buffer[1]
+
+        joined_grid = []
+        for gdf in grid:
+            # A single bbox is enough to reconstruct the grid tile bbox.
+            reduced_gdf = gdf.groupby(by="name").first().reset_index()
+
+            reduced_gdf.BBOX = reduced_gdf.BBOX.apply(lambda bbox: bbox.buffer(reversed_buffer, relative=False))
+            reduced_gdf.BBOX = reduced_gdf.filter(["BBOX", "split_x", "split_y"]).apply(
+                lambda row: get_grid_bbox(row.BBOX, (row.split_x, row.split_y), split), axis=1
+            )
+
+            # Performs a numerical correction because it assumes the base batch grid is in UTM and all coordinates are
+            # aligned to integers.
+            reduced_gdf.BBOX = reduced_gdf.BBOX.apply(convert_bbox_coords_to_int)
+
+            joined_grid.append(reduced_gdf)
+
+        return joined_grid
+
+    def _split_batch_grid(
+        self, grid: List[GeoDataFrame], split: Tuple[int, int], buffer: Tuple[float, float]
+    ) -> List[GeoDataFrame]:
+        """Splits tiles of batch grid into sub-tiles and applies buffer."""
+        new_tiles = []
+        for gdf in grid:
+            for _, row in gdf.iterrows():
+                tile_bbox: BBox = row.BBOX
+                partition = tile_bbox.get_partition(num_x=split[0], num_y=split[1])
+
+                for index_x, bbox_list in enumerate(partition):
+                    for index_y, bbox in enumerate(bbox_list):
+                        tile_info = {
+                            "name": row["name"],
+                            "id": row["id"],
+                            "split_x": index_x,
+                            "split_y": index_y,
+                            "BBOX": bbox.buffer(buffer, relative=False),
+                        }
+                        new_tiles.append(tile_info)
+
+        bbox_list = [item["BBOX"] for item in new_tiles]
+        return self._to_dataframe_grid(bbox_list, new_tiles, self._BATCH_GRID_COLUMNS + ["BBOX"])
+
+    def _filter_grid_with_area(self, grid: List[GeoDataFrame]) -> List[GeoDataFrame]:
+        """Removes those (sub)tiles in the grid that don't intersect with the area of interest."""
+        area_geometry = self.get_area_geometry(ignore_region_filter=True)
+
+        filtered_grid = []
+        for gdf in grid:
+            transformed_area_geometry = area_geometry.transform(CRS(gdf.crs))
+            gdf = gdf[gdf.intersects(transformed_area_geometry.geometry)]
+            filtered_grid.append(gdf)
+
+        return filtered_grid
+
 
 class MissingBatchIdError(ValueError):
     """Exception that is triggered if ID of a Sentinel Hub batch job is missing."""
+
+
+def _fix_split_columns(grid: List[GeoDataFrame]) -> List[GeoDataFrame]:
+    """This is temporary fix for handling batch grids that were cached with older eo-grow version."""
+    columns_missing = False
+    for gdf in grid:
+        for column_name in ["split_x", "split_y"]:
+            if column_name not in gdf:
+                gdf[column_name] = 0
+                columns_missing = True
+
+    if columns_missing:
+        warnings.warn(
+            "Since eo-grow 1.1.0 cached batch grid should also have columns split_x and split_y. "
+            "For the future, make sure to clear cached grid file and reproduce it.",
+            category=EODeprecationWarning,
+        )
+
+    return grid
--- eogrow/core/area/utm.py
+++ eogrow/core/area/utm.py
@@ -2,13 +2,15 @@
 Area managers working with UTM CRS
 """
 import logging
-from typing import Any, List
+from typing import Any, List, Tuple, cast
 
 from geopandas import GeoDataFrame
 from pydantic import Field
 
 from sentinelhub import Geometry, UtmZoneSplitter
 
+from ...utils.general import reduce_to_coprime
+from ...utils.grid import GridTransformation, create_transformations
 from .base import AreaManager
 
 LOGGER = logging.getLogger(__name__)
@@ -67,3 +69,59 @@ class UtmZoneAreaManager(AreaManager):
             tile_info["total_num"] = len(bbox_list)
 
         return self._to_dataframe_grid(bbox_list, info_list, ["index_n", "index_x", "index_y", "total_num"])
+
+    def transform_grid(self, target_area_manager: AreaManager) -> List[GridTransformation]:
+        """Calculates how grid should be transformed into another area manager."""
+        self._check_target_compatibility(target_area_manager)
+        target_area_manager = cast(UtmZoneAreaManager, target_area_manager)
+
+        source_grid = self.get_grid(add_bbox_column=True)
+        target_grid = target_area_manager.get_grid(add_bbox_column=True)
+
+        source_size = self.config.patch_size_x, self.config.patch_size_y
+        target_size = target_area_manager.config.patch_size_x, target_area_manager.config.patch_size_y
+
+        sizes_x, sizes_y = zip(source_size, target_size)
+        target_shape, source_shape = zip(reduce_to_coprime(*sizes_x), reduce_to_coprime(*sizes_y))
+
+        source_grid = self._add_patch_positions(source_grid, self.config, source_shape)
+        target_grid = self._add_patch_positions(target_grid, target_area_manager.config, target_shape)
+
+        return create_transformations(source_grid, target_grid, match_columns=["position_x", "position_y"])
+
+    @staticmethod
+    def _add_patch_positions(grid: List[GeoDataFrame], config: Schema, shape: Tuple[int, int]) -> List[GeoDataFrame]:
+        """Adds columns of values that define how sub-tiles should be spatially divided into groups. Members of each
+        group will have the same values in the newly added columns and will spatially form a regular subgrid of given
+        shape.
+
+        The process works with the lower left vertex of the bounding box. First it reverts buffer and offset shift.
+        This way each vertex will have coordinates that are an integer multiple of patch size because this is the way
+        `UtmZoneSplitter` created the grid. In the next step the vertex values are divided by patch size and the given
+        shape of the subgroup. This produces exactly the desired indices that define groups.
+        """
+        translate_x = config.patch_buffer_x - config.offset_x
+        translate_y = config.patch_buffer_y - config.offset_y
+        scale_x = config.patch_size_x * shape[0]
+        scale_y = config.patch_size_y * shape[1]
+
+        for gdf in grid:
+            gdf["position_x"] = gdf.BBOX.apply(lambda bbox: round(bbox.min_x + translate_x) // scale_x)
+            gdf["position_y"] = gdf.BBOX.apply(lambda bbox: round(bbox.min_y + translate_y) // scale_y)
+
+        return grid
+
+    def _check_target_compatibility(self, target_area_manager: AreaManager) -> None:
+        """Checks if source and target managers are compatible to define a transformation between them."""
+        if not isinstance(target_area_manager, UtmZoneAreaManager):
+            raise NotImplementedError(
+                f"Grid transformation is only supported into another {self.__class__.__name__}, but got "
+                f"{target_area_manager.__class__.__name__}"
+            )
+
+        for param in ["offset_x", "offset_y"]:
+            if getattr(self.config, param) != getattr(target_area_manager.config, param):
+                raise NotImplementedError(
+                    "Cannot transform grid because source and target area managers have different values of config "
+                    f"parameter {param}."
+                )
