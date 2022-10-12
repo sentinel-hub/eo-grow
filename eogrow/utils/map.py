1e81bebdc1f8a728ee24b199cc22ca0234fab7f9
Ziga Luksic <ziga.luksic@sinergise.com>
2022-10-12 14:34:34 +0200

    fix extract bands

--- eogrow/utils/map.py
+++ eogrow/utils/map.py
@@ -149,7 +149,7 @@ def extract_bands(input_file: str, output_file: str, bands: Sequence[int], overw
             raise OSError(f"{output_file} already exists. Set `overwrite` to true if it should be overwritten.")
 
     # gdal_translate starts indexation at 1
-    translate_opts = "-co compress=LZW" + " ".join(f" -b {band - 1}" for band in bands)
+    translate_opts = "-co compress=LZW" + " ".join(f" -b {band + 1}" for band in bands)
 
     temp_filename = NamedTemporaryFile()
     temp_filename.close()
