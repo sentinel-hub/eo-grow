5b0b4cb3edabc6b3179cfb29403508f9da296bbb
Ziga Luksic <ziga.luksic@sinergise.com>
2022-10-18 09:51:10 +0200

    adjust description

--- eogrow/pipelines/export_maps.py
+++ eogrow/pipelines/export_maps.py
@@ -64,8 +64,8 @@ class ExportMapsPipeline(Pipeline):
         compress_temporally: bool = Field(
             False,
             description=(
-                "Temporal features are by default exported as multiple TIFF files in a per-timestamp manner. This "
-                "parameter results in a single TIFF file with the same band order as the one of `ExportToTiffTask`."
+                "Temporal features are by default exported as multiple TIFF files in a per-timestamp manner. Enabling "
+                "this parameter results in a single TIFF with the same band order as the one of `ExportToTiffTask`."
             ),
         )
         skip_existing: Literal[False] = False
