b9335fbf8b9619828c2e41ca90b55d5c9d92fac9
Ziga Luksic <ziga.luksic@sinergise.com>
2022-11-11 10:25:23 +0100

    slight test reorganization

--- tests/test_pipelines/test_mapping.py
+++ tests/test_pipelines/test_mapping.py
@@ -11,14 +11,7 @@ def config_folder_fixture(config_folder, stats_folder):
     return create_folder_dict(config_folder, stats_folder, "mapping")
 
 
-@pytest.mark.chain
-@pytest.mark.order(after=["test_rasterize.py::test_rasterize_pipeline_features"])
-@pytest.mark.parametrize("experiment_name", ["mapping_ref"])
-def test_mapping_pipeline_on_reference_data(experiment_name, folders):
-    run_and_test_pipeline(experiment_name, **folders)
-
-
 @pytest.mark.order(after=["test_prediction.py::test_prediction_pipeline"])
-@pytest.mark.parametrize("experiment_name", ["mapping_pred"])
-def test_mapping_pipeline_on_predictions(experiment_name, folders):
+@pytest.mark.parametrize("experiment_name", ["mapping_ref", pytest.param("mapping_pred", marks=pytest.mark.chain)])
+def test_mapping_pipeline_on_reference_data(experiment_name, folders):
     run_and_test_pipeline(experiment_name, **folders)
