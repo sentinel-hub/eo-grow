0cc821d1b687d28eb4c2cb88e0caaeb5721be26a
Ziga Luksic <ziga.luksic@sinergise.com>
2022-11-28 14:06:42 +0100

    improve readability of zipmap in utils and correct test

--- eogrow/utils/zipmap.py
+++ eogrow/utils/zipmap.py
@@ -1,8 +1,18 @@
 """A collection of functions to be used with the ZipMapPipeline."""
 
-from typing import Dict, Optional, Union
+from typing import Dict, Optional
 
 import numpy as np
+
+from ..core.schemas import BaseSchema
+from .validators import optional_field_validator, parse_dtype
+
+
+class MapParams(BaseSchema):
+    mapping: Dict[int, int]
+    default: Optional[int]
+    dtype: np.dtype
+    _parse_dtype = optional_field_validator("dtype", parse_dtype, pre=True)
 
 
 def map_values(
@@ -10,13 +20,16 @@ def map_values(
     *,
     mapping: Dict[int, int],
     default: Optional[int] = None,
-    dtype: Union[None, str, np.dtype, type] = None
+    dtype: Optional[np.dtype] = None,
 ) -> np.ndarray:
     """Maps all values from `array` according to a dictionary. A default value can be given, which is assigned to
     values without a corresponding key in the mapping dictionary.
 
     Vectorizing the `.get` method is faster, but difficult to do with a default (and also loses speed advantage)."""
-    int_mapping = {int(k): v for k, v in mapping.items()}
+
     if default is not None:
-        return np.array([int_mapping.get(x, default) for x in array.ravel()], dtype=dtype).reshape(array.shape)
-    return np.vectorize(int_mapping.get)(array).astype(dtype)
+        flat_mapped_array = np.array([mapping.get(x, default) for x in array.ravel()], dtype=dtype)
+        return flat_mapped_array.reshape(array.shape)
+
+    mapped_array = np.vectorize(mapping.get)(array)
+    return mapped_array.astype(dtype)
