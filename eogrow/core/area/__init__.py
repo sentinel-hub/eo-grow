267130d992ee5d79ce9202082b690ce6d75da9b5
Ziga Luksic <ziga.luksic@sinergise.com>
2022-12-05 15:02:19 +0100

    add new utm area manager implementation

--- eogrow/core/area/__init__.py
+++ eogrow/core/area/__init__.py
@@ -4,4 +4,4 @@ A subfolder for area management
 
 from .batch import BatchAreaManager
 from .custom_grid import CustomGridAreaManager, NewCustomGridAreaManager
-from .utm import UtmZoneAreaManager
+from .utm import NewUtmZoneAreaManager, UtmZoneAreaManager
