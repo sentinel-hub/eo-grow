4cccd15510f90aa4945f6d43763d3ea05c0cb0e9
jgersak <jan.gersak@sinergise.com>
2023-01-24 16:06:20 +0100

    Update test_schemas
    Add ZipMapPipeline to test_schemas

--- tests/test_core/test_schemas.py
+++ tests/test_core/test_schemas.py
@@ -4,12 +4,13 @@ from eogrow.core.area import UtmZoneAreaManager
 from eogrow.core.schemas import build_schema_template
 from eogrow.core.storage import StorageManager
 from eogrow.pipelines.export_maps import ExportMapsPipeline
+from eogrow.pipelines.zipmap import ZipMapPipeline
 
 pytestmark = pytest.mark.fast
 
 
 @pytest.mark.fast
-@pytest.mark.parametrize("eogrow_object", [UtmZoneAreaManager, ExportMapsPipeline, StorageManager])
+@pytest.mark.parametrize("eogrow_object", [UtmZoneAreaManager, ZipMapPipeline, ExportMapsPipeline, StorageManager])
 def test_build_schema_template(eogrow_object):
     template = build_schema_template(eogrow_object.Schema)
     assert isinstance(template, dict)
