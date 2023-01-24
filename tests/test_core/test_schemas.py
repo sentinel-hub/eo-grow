02d87fb4ae8b6144bd5619a3d87c48103cbfec60
jgersak <jan.gersak@sinergise.com>
2023-01-24 15:53:48 +0100

    Remove MappingPipeline
    remove MappingPipeline
    remove MappingTask
    update accordingly

--- tests/test_core/test_schemas.py
+++ tests/test_core/test_schemas.py
@@ -4,13 +4,12 @@ from eogrow.core.area import UtmZoneAreaManager
 from eogrow.core.schemas import build_schema_template
 from eogrow.core.storage import StorageManager
 from eogrow.pipelines.export_maps import ExportMapsPipeline
-from eogrow.pipelines.mapping import MappingPipeline
 
 pytestmark = pytest.mark.fast
 
 
 @pytest.mark.fast
-@pytest.mark.parametrize("eogrow_object", [UtmZoneAreaManager, MappingPipeline, ExportMapsPipeline, StorageManager])
+@pytest.mark.parametrize("eogrow_object", [UtmZoneAreaManager, ExportMapsPipeline, StorageManager])
 def test_build_schema_template(eogrow_object):
     template = build_schema_template(eogrow_object.Schema)
     assert isinstance(template, dict)
