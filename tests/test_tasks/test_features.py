f79dfeed5cef7799e9393098ed6bae7f7e5cdf67
jgersak <jan.gersak@sinergise.com>
2023-01-25 16:26:07 +0100

    rename test cases add not symmetrical test case

--- tests/test_tasks/test_features.py
+++ tests/test_tasks/test_features.py
@@ -7,22 +7,22 @@ from eogrow.tasks.features import ValidDataFractionPredicate
 
 pytestmark = pytest.mark.fast
 
-ZEROS = np.zeros((1, 100, 100))
+ZEROES = np.zeros((1, 100, 100))
 ONES = np.ones((1, 100, 100))
-MIX_CLEAN = np.concatenate([np.zeros((1, 100, 100)), np.ones((1, 100, 100))])
-MIX_WIRD = np.concatenate([np.zeros((10000)), np.ones((10000))])
-np.random.shuffle(MIX_WIRD)
-MIX_WIRD.reshape(2, 100, 100)
-MIX_BOOL = np.array(MIX_WIRD, dtype=bool)
+NOT_SYMMETRICAL = np.array([x % 3 == 0 for x in range(1000)]).reshape(10, 10, 10)
+MIX = np.concatenate([np.zeros((10000)), np.ones((10000))])
+np.random.shuffle(MIX)
+MIX.reshape(2, 100, 100)
+MIX_BOOL = MIX.astype(bool)
 
 
 @pytest.mark.parametrize(
     "threshold, test_cases",
     [
-        (0.0, [(ZEROS, False), (MIX_CLEAN, True), (MIX_WIRD, True), (MIX_BOOL, True), (ONES, True)]),
-        (0.49, [(ZEROS, False), (MIX_CLEAN, True), (MIX_WIRD, True), (MIX_BOOL, True), (ONES, True)]),
-        (0.5, [(ZEROS, False), (MIX_CLEAN, False), (MIX_WIRD, False), (MIX_BOOL, False), (ONES, True)]),
-        (1.0, [(ZEROS, False), (MIX_CLEAN, False), (MIX_WIRD, False), (MIX_BOOL, False), (ONES, False)]),
+        (0.0, [(ZEROES, False), (NOT_SYMMETRICAL, True), (MIX, True), (MIX_BOOL, True), (ONES, True)]),
+        (0.49, [(ZEROES, False), (NOT_SYMMETRICAL, False), (MIX, True), (MIX_BOOL, True), (ONES, True)]),
+        (0.5, [(ZEROES, False), (NOT_SYMMETRICAL, False), (MIX, False), (MIX_BOOL, False), (ONES, True)]),
+        (1.0, [(ZEROES, False), (NOT_SYMMETRICAL, False), (MIX, False), (MIX_BOOL, False), (ONES, False)]),
     ],
 )
 def test_ValidDataFractionPredicate(threshold: float, test_cases: List[Tuple[np.ndarray, bool]]) -> None:
