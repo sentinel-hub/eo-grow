b6de643ed4c7f0e6b8f62ef8540d3c14b3bcf485
jgersak <jan.gersak@sinergise.com>
2023-01-25 16:10:41 +0100

    Update MIX_WIRD case add MIX_BOOL case

--- tests/test_tasks/test_features.py
+++ tests/test_tasks/test_features.py
@@ -10,17 +10,19 @@ pytestmark = pytest.mark.fast
 ZEROS = np.zeros((1, 100, 100))
 ONES = np.ones((1, 100, 100))
 MIX_CLEAN = np.concatenate([np.zeros((1, 100, 100)), np.ones((1, 100, 100))])
-MIX_WIRD = np.concatenate([np.zeros((1, 100, 100)), np.ones((1, 100, 100))])
+MIX_WIRD = np.concatenate([np.zeros((10000)), np.ones((10000))])
 np.random.shuffle(MIX_WIRD)
+MIX_WIRD.reshape(2, 100, 100)
+MIX_BOOL = np.array(MIX_WIRD, dtype=bool)
 
 
 @pytest.mark.parametrize(
     "threshold, test_cases",
     [
-        (0.0, [(ZEROS, False), (MIX_CLEAN, True), (MIX_WIRD, True), (ONES, True)]),
-        (0.49, [(ZEROS, False), (MIX_CLEAN, True), (MIX_WIRD, True), (ONES, True)]),
-        (0.5, [(ZEROS, False), (MIX_CLEAN, False), (MIX_WIRD, False), (ONES, True)]),
-        (1.0, [(ZEROS, False), (MIX_CLEAN, False), (MIX_WIRD, False), (ONES, False)]),
+        (0.0, [(ZEROS, False), (MIX_CLEAN, True), (MIX_WIRD, True), (MIX_BOOL, True), (ONES, True)]),
+        (0.49, [(ZEROS, False), (MIX_CLEAN, True), (MIX_WIRD, True), (MIX_BOOL, True), (ONES, True)]),
+        (0.5, [(ZEROS, False), (MIX_CLEAN, False), (MIX_WIRD, False), (MIX_BOOL, False), (ONES, True)]),
+        (1.0, [(ZEROS, False), (MIX_CLEAN, False), (MIX_WIRD, False), (MIX_BOOL, False), (ONES, False)]),
     ],
 )
 def test_ValidDataFractionPredicate(threshold: float, test_cases: List[Tuple[np.ndarray, bool]]) -> None:
