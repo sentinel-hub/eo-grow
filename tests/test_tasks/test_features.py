b2bbea509f80e0b28cdf2151d505646b391fcbcf
jgersak <jan.gersak@sinergise.com>
2023-01-25 15:18:55 +0100

    Update tests
    remove MaxNDVI test
    update test ValidDataFractionPredicate

--- tests/test_tasks/test_features.py
+++ tests/test_tasks/test_features.py
@@ -1,39 +1,29 @@
+from typing import List, Tuple
+
 import numpy as np
 import pytest
-from numpy.testing import assert_array_equal
 
-from eolearn.core import EOPatch
-from eolearn.core.constants import FeatureType
+from eogrow.tasks.features import ValidDataFractionPredicate
 
-from eogrow.tasks.features import MaxNDVI, ValidDataFractionPredicate
+pytestmark = pytest.mark.fast
 
-
-@pytest.mark.parametrize(
-    "validity_threshold, data, expected",
-    [
-        (0.0, np.array([]), False),
-        (0.9, np.array([True], dtype=bool), True),
-        (0.0, np.zeros((3, 3, 2)), False),
-        (0.45, np.concatenate([np.zeros(100), np.ones(100)]).reshape(2, 10, 10), True),
-        (0.5, np.concatenate([np.zeros(100), np.ones(100)]).reshape(2, 10, 10), False),
-    ],
-)
-def test_ValidDataFractionPredicate(validity_threshold: float, data: np.ndarray, expected: bool) -> None:
-    test_predicate = ValidDataFractionPredicate(validity_threshold)
-    assert test_predicate(data) == expected
+ZEROS = np.zeros((1, 100, 100))
+ONES = np.ones((1, 100, 100))
+MIX_CLEAN = np.concatenate([np.zeros((1, 100, 100)), np.ones((1, 100, 100))])
+MIX_WIRD = np.concatenate([np.zeros((1, 100, 100)), np.ones((1, 100, 100))])
+np.random.shuffle(MIX_WIRD)
 
 
 @pytest.mark.parametrize(
-    "ndvi, expected",
+    "threshold, test_cases",
     [
-        (np.ones((1, 1, 1, 1)), np.ones((1, 1, 1))),
-        (np.ones((5, 10, 10, 1)), np.ones((10, 10, 1))),
-        (np.arange(500).reshape((5, 10, 10, 1)), np.arange(400, 500).reshape((10, 10, 1))),
+        (0.0, [(ZEROS, False), (MIX_CLEAN, True), (MIX_WIRD, True), (ONES, True)]),
+        (0.49, [(ZEROS, False), (MIX_CLEAN, True), (MIX_WIRD, True), (ONES, True)]),
+        (0.5, [(ZEROS, False), (MIX_CLEAN, False), (MIX_WIRD, False), (ONES, True)]),
+        (1.0, [(ZEROS, False), (MIX_CLEAN, False), (MIX_WIRD, False), (ONES, False)]),
     ],
 )
-def test_MaxNDVI(ndvi: np.ndarray, expected: np.ndarray) -> None:
-    patch = EOPatch()
-    patch.data["NDVI"] = ndvi
-
-    test_max_ndvi = MaxNDVI((FeatureType.DATA, "NDVI"), (FeatureType.DATA_TIMELESS, "MaxNDVI"))
-    assert_array_equal(test_max_ndvi(patch).data_timeless["MaxNDVI"], expected)
+def test_ValidDataFractionPredicate(threshold: float, test_cases: List[Tuple[np.ndarray, bool]]) -> None:
+    test_predicate = ValidDataFractionPredicate(threshold)
+    for case in test_cases:
+        assert test_predicate(case[0]) == case[1]
