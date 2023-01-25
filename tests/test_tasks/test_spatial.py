7c204e1e2fb61045f8c8e14b466fbfcd7c44b22e
jgersak <jan.gersak@sinergise.com>
2023-01-25 12:07:44 +0100

    Update
    update docstrings to spatial
    update tests
     + remove duplicated fully covered test case
     + update some bboxes so they don't align in the lower left corner
     + update wird size limit test case
       + mark unintendent behaviour in skiped test
     + add constat for repeated BBox

--- tests/test_tasks/test_spatial.py
+++ tests/test_tasks/test_spatial.py
@@ -6,6 +6,8 @@ from sentinelhub import CRS, BBox
 
 from eogrow.tasks.spatial import get_array_slices
 
+pytestmark = pytest.mark.fast
+
 
 @pytest.mark.parametrize(
     "input_params, expected",
@@ -13,19 +15,10 @@ from eogrow.tasks.spatial import get_array_slices
         pytest.param(
             {
                 "bbox": BBox(((730000, 4400000), (731000, 4401000)), crs=CRS("32638")),
-                "slice_bbox": BBox(((730000, 4400000), (731000, 4401000)), crs=CRS("32638")),
-                "resolution": (10, 10),
-            },
-            (slice(0, 100, None), slice(0, 100, None)),
-            id="fully covered",
-        ),
-        pytest.param(
-            {
-                "bbox": BBox(((730000, 4400000), (731000, 4401000)), crs=CRS("32638")),
                 "slice_bbox": BBox(((730000, 4400000), (730990, 4400650)), crs=CRS("32638")),
                 "resolution": (10, 10),
             },
-            (slice(0, 99, None), slice(0, 65, None)),
+            (slice(0, 99, None), slice(35, 100, None)),
             id="contained",
         ),
         pytest.param(
@@ -35,7 +28,7 @@ from eogrow.tasks.spatial import get_array_slices
                 "resolution": (10, 10),
             },
             (slice(0, 200, None), slice(0, 100, None)),
-            id="random test",
+            id="fully covered",
         ),
         pytest.param(
             {
@@ -51,20 +44,50 @@ from eogrow.tasks.spatial import get_array_slices
         pytest.param(
             {
                 "bbox": BBox(bbox=[500, 500, 600, 600], crs=CRS.WGS84),
-                "slice_bbox": BBox(bbox=[500, 500, 600, 600], crs=CRS.WGS84),
+                "slice_bbox": BBox(bbox=[520, 520, 580, 590], crs=CRS.WGS84),
                 "resolution": (10, 10),
             },
-            (slice(0, 10, None), slice(0, 10, None)),
+            (slice(2, 8, None), slice(1, 8, None)),
             id="test WGS84",
         ),
         pytest.param(
             {
                 "bbox": BBox(((730000, 4400000), (732000, 4401000)), crs=CRS("32638")),
-                "slice_bbox": BBox(((730000, 4400000), (732000, 4401000)), crs=CRS("32638")),
+                "slice_bbox": BBox(((731000, 4400000), (732000, 4400500)), crs=CRS("32638")),
                 "size": (10, 10),
             },
-            (slice(0, 10, None), slice(0, 10, None)),
+            (slice(5, 10, None), slice(5, 10, None)),
             id="test size",
+        ),
+        pytest.param(
+            {
+                "bbox": BBox(((730000, 4400000), (732000, 4401000)), crs=CRS("32638")),
+                "slice_bbox": BBox(((730000, 4400000), (732000, 4401000)), crs=CRS("32638")),
+                "size": (100, 100),
+                "limit_x": (50, 100),
+                "limit_y": (100, 200),
+            },
+            (slice(50, 100, None), slice(100, 100, None)),
+            id="test size limit",
+        ),
+        pytest.param(
+            {
+                "bbox": BBox(((730000, 4400000), (732000, 4401000)), crs=CRS("32638")),
+                "slice_bbox": BBox(((730100, 4400100), (731900, 4400800)), crs=CRS("32638")),
+                "resolution": (10, 10),
+            },
+            (slice(10, 190, None), slice(20, 90, None)),
+            id="test slice_bbox included",
+        ),
+        # This test is specific to the current implementation, and serves only to detect unplanned changes.
+        pytest.param(
+            {
+                "bbox": BBox(((730000, 4400000), (732000, 4401000)), crs=CRS("32638")),
+                "slice_bbox": BBox(((731000, 4400500), (733000, 4401500)), crs=CRS("32638")),
+                "resolution": (10, 10),
+            },
+            (slice(100, 300, None), slice(-50, 50, None)),
+            id="test slice_bbox intersection",
         ),
         pytest.param(
             {
@@ -75,7 +98,8 @@ from eogrow.tasks.spatial import get_array_slices
                 "limit_y": (100, 200),
             },
             (slice(50, 10, None), slice(100, 10, None)),
-            id="test size limit",
+            id="unintended behaviour",
+            marks=pytest.mark.skip("unintended behaviour"),
         ),
     ],
 )
@@ -83,20 +107,23 @@ def test_get_array_slices(input_params: Dict[str, Any], expected: Tuple[slice, s
     assert get_array_slices(**input_params) == expected
 
 
+BBOX_FOR_INVALID_INPUT: BBox = BBox(((730000, 4400000), (732000, 4401000)), crs=CRS("32638"))
+
+
 @pytest.mark.parametrize(
     "input_params",
     [
         pytest.param(
             {
-                "bbox": BBox(((730000, 4400000), (732000, 4401000)), crs=CRS("32638")),
-                "slice_bbox": BBox(((730000, 4400000), (732000, 4401000)), crs=CRS("32638")),
+                "bbox": BBOX_FOR_INVALID_INPUT,
+                "slice_bbox": BBOX_FOR_INVALID_INPUT,
             },
             id="no resolution or size",
         ),
         pytest.param(
             {
-                "bbox": BBox(((730000, 4400000), (732000, 4401000)), crs=CRS("32638")),
-                "slice_bbox": BBox(((730000, 4400000), (732000, 4401000)), crs=CRS("32638")),
+                "bbox": BBOX_FOR_INVALID_INPUT,
+                "slice_bbox": BBOX_FOR_INVALID_INPUT,
                 "resolution": (10, 10),
                 "size": (10, 10),
             },
@@ -104,7 +131,7 @@ def test_get_array_slices(input_params: Dict[str, Any], expected: Tuple[slice, s
         ),
         pytest.param(
             {
-                "bbox": BBox(((730000, 4400000), (732000, 4401000)), crs=CRS("32638")),
+                "bbox": BBOX_FOR_INVALID_INPUT,
                 "slice_bbox": BBox(((730000.5, 4400000.5), (732000, 4401000)), crs=CRS("32638")),
                 "resolution": (10, 10),
             },
