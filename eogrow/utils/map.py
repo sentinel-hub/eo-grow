9dd2b54acadf710cba322270d3f446757a3ecee4
Ashish Dhiman <Ashish.Dhiman@tomtom.com>
2023-03-06 13:50:18 +0100

    Pre-Hook run & review comments addressed

--- eogrow/utils/map.py
+++ eogrow/utils/map.py
@@ -5,10 +5,11 @@ import logging
 import os
 import shutil
 import subprocess
+import tempfile
 import warnings
+from pathlib import Path
 from tempfile import NamedTemporaryFile
 from typing import Iterable, Literal, Optional
-from pathlib import Path
 
 SH_COMMAND_LIMIT = 130000
 OPEN_FILES_LIMIT = 1000
@@ -184,19 +185,23 @@ def merge_tiffs(
     command = f"gdalwarp {gdalwarp_options} {' '.join(input_filelist)} {merged_filename}"
 
     if len(command) > SH_COMMAND_LIMIT or len(input_filelist) > OPEN_FILES_LIMIT:
-        # generate a text file with
         merged_path = Path(merged_filename)
-        tile_list_path = str(merged_path.with_name(f"{merged_path.stem}_files.txt"))
-        vrt_file_path = str(merged_path.with_name(f"{merged_path.stem}_temp.vrt"))
-        with open(tile_list_path, "w") as file:
-            for string in input_filelist:
-                file.write(string + "\n")
-        generate_vrt_command = f"gdalbuildvrt {vrt_file_path} -input_file_list {tile_list_path}"
+        vrt_file_path = merged_path.with_name(f"{merged_path.stem}_temp.vrt")
+        LOGGER.info(f"Command too big or too many files to process. Creating an intermediary vrt: {vrt_file_path}")
+        # generate text file with tile names & generate vrt
+        with tempfile.NamedTemporaryFile(mode="w", delete=True) as file_list:
+            file_list.writelines([f"{tname}\n" for tname in input_filelist])
+            generate_vrt_command = f"gdalbuildvrt {vrt_file_path} -input_file_list {file_list.name}"
+            subprocess.check_call(generate_vrt_command, shell=True)
 
-        LOGGER.info(f"Command too big or Too many files to process. Creating an intermediary vrt: {vrt_file_path}")
-        subprocess.check_call(generate_vrt_command, shell=True)
+        # create merged file
         command = f"gdalwarp {gdalwarp_options} {vrt_file_path} {merged_filename}"
-    subprocess.check_call(command, shell=True)
+        subprocess.check_call(command, shell=True)
+
+        # cleanup the vrt after the process
+        os.remove(vrt_file_path)
+    else:
+        subprocess.check_call(command, shell=True)
 
 
 def extract_bands(
