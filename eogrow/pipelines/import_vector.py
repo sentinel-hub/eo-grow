fb8a66e6d80e9a00bf14df15a628c5435b2bb9b9
Ziga Luksic <ziga.luksic@sinergise.com>
2023-03-24 11:02:29 +0100

    add pipeline and tests for timeless version

---
+++ eogrow/pipelines/import_vector.py
@@ -0,0 +1,53 @@
+"""Implements a pipeline for importing vector data from a file."""
+
+import fs
+from pydantic import Field
+
+from eolearn.core import EOWorkflow, FeatureType, OverwritePermission, SaveTask, linearly_connect_tasks
+from eolearn.io import VectorImportTask
+
+from ..core.pipeline import Pipeline
+from ..types import ExecKwargs, Feature, PatchList
+
+
+class ImportVectorPipeline(Pipeline):
+    class Schema(Pipeline.Schema):
+        input_folder_key: str = Field("input_data", description="The folder key into which the EOPatch will be saved. ")
+        input_filename: str = Field(
+            description="Filename of the vector file to be imported. Needs to be located in the input-data folder."
+        )
+        reproject: bool = Field(True, description="Controls whether the vector file is reprojected to the EOPatch CRS.")
+        clip: bool = Field(True, description="Controls whether the polygons are clipped to the EOPatch boundaries. ")
+        output_feature: Feature = Field(description="The EOPatch feature to which the vector will be imported.")
+        output_folder_key: str = Field(description="The folder key into which the EOPatch will be saved. ")
+
+    config: Schema
+
+    def get_execution_arguments(self, workflow: EOWorkflow, patch_list: PatchList) -> ExecKwargs:
+        exec_kwargs = super().get_execution_arguments(workflow, patch_list)
+        vector_node = next(node for node in workflow.get_nodes() if isinstance(node.task, VectorImportTask))
+
+        for name, bbox in patch_list:
+            exec_kwargs[name][vector_node] = dict(bbox=bbox)
+
+        return exec_kwargs
+
+    def build_workflow(self) -> EOWorkflow:
+        path = fs.path.join(self.storage.get_input_data_folder(), self.config.input_filename)
+
+        vector_import_task = VectorImportTask(
+            feature=self.config.output_feature,
+            path=path,
+            reproject=self.config.reproject,
+            clip=self.config.clip,
+            filesystem=self.storage.filesystem,
+        )
+
+        save_task = SaveTask(
+            path=self.storage.get_folder(self.config.output_folder_key),
+            filesystem=self.storage.filesystem,
+            features=[FeatureType.BBOX, self.config.output_feature],
+            compress_level=1,
+            overwrite_permission=OverwritePermission.OVERWRITE_FEATURES,
+        )
+        return EOWorkflow(linearly_connect_tasks(vector_import_task, save_task))
---
+++ tests/test_pipelines/test_import_vector.py
@@ -0,0 +1,15 @@
+import pytest
+
+from eogrow.utils.testing import create_folder_dict, run_and_test_pipeline
+
+pytestmark = pytest.mark.integration
+
+
+@pytest.fixture(scope="session", name="folders")
+def config_folder_fixture(config_folder, stats_folder):
+    return create_folder_dict(config_folder, stats_folder, "import_vector")
+
+
+@pytest.mark.parametrize("experiment_name", ["import_vector"])
+def test_import_tiff_pipeline(folders, experiment_name):
+    run_and_test_pipeline(experiment_name, **folders)
