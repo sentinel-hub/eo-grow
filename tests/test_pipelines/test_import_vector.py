70131018e103d7b2a7f68261c5e00c15616ee485
Ziga Luksic <ziga.luksic@sinergise.com>
2023-03-24 12:34:33 +0100

    mark tests as part of chain

--- tests/test_pipelines/test_import_vector.py
+++ tests/test_pipelines/test_import_vector.py
@@ -10,6 +10,7 @@ def config_folder_fixture(config_folder, stats_folder):
     return create_folder_dict(config_folder, stats_folder, "import_vector")
 
 
+@pytest.mark.chain
 @pytest.mark.parametrize("experiment_name", ["import_vector", "import_vector_temporal"])
 def test_import_tiff_pipeline(folders, experiment_name):
     run_and_test_pipeline(experiment_name, **folders)
