32328de10cb2778d53222da1956e28acbac6d097
Ziga Luksic <ziga.luksic@sinergise.com>
2023-04-06 14:08:03 +0200

    adjust zipmap and byoc tests

--- tests/test_pipelines/test_zipmap.py
+++ tests/test_pipelines/test_zipmap.py
@@ -1,16 +1,13 @@
 import pytest
 
-from eogrow.utils.testing import create_folder_dict, run_and_test_pipeline
+from eogrow.utils.testing import compare_content, run_config
 
 pytestmark = pytest.mark.integration
 
 
-@pytest.fixture(scope="session", name="folders")
-def config_folder_fixture(config_folder, stats_folder):
-    return create_folder_dict(config_folder, stats_folder, "zipmap")
-
-
 @pytest.mark.order(after=["test_prediction.py::test_rasterization_pipeline"])
 @pytest.mark.parametrize("experiment_name", [pytest.param("zipmap", marks=pytest.mark.chain)])
-def test_zipmap_pipeline(experiment_name, folders):
-    run_and_test_pipeline(experiment_name, **folders)
+def test_zipmap_pipeline(config_and_stats_paths, experiment_name):
+    config_path, stats_path = config_and_stats_paths("zipmap", experiment_name)
+    run_config(config_path)
+    compare_content(config_path, stats_path)
