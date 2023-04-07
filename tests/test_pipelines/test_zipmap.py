7bc8cae9b2d04c030d71462e7de9ccc0927ee307
Ziga Luksic <ziga.luksic@sinergise.com>
2023-04-07 12:09:16 +0200

    fix odd old bug in zipmap

--- tests/test_pipelines/test_zipmap.py
+++ tests/test_pipelines/test_zipmap.py
@@ -5,7 +5,7 @@ from eogrow.utils.testing import compare_content, run_config
 pytestmark = pytest.mark.integration
 
 
-@pytest.mark.order(after=["test_prediction.py::test_rasterization_pipeline"])
+@pytest.mark.order(after=["test_rasterize.py::test_rasterize_pipeline"])
 @pytest.mark.parametrize("experiment_name", [pytest.param("zipmap", marks=pytest.mark.chain)])
 def test_zipmap_pipeline(config_and_stats_paths, experiment_name):
     config_path, stats_path = config_and_stats_paths("zipmap", experiment_name)
