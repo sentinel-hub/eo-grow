10ad9d42cf818af2535d7edcfe42b3ccb1a0733e
Matic Lubej <matic.lubej@sinergise.com>
2023-04-20 11:12:31 +0200

    fix test name

--- tests/test_pipelines/test_import_vector.py
+++ tests/test_pipelines/test_import_vector.py
@@ -7,7 +7,7 @@ pytestmark = pytest.mark.integration
 
 @pytest.mark.chain
 @pytest.mark.parametrize("experiment_name", ["import_vector", "import_vector_temporal"])
-def test_import_tiff_pipeline(config_and_stats_paths, experiment_name):
+def test_import_vector_pipeline(config_and_stats_paths, experiment_name):
     config_path, stats_path = config_and_stats_paths("import_vector", experiment_name)
     output_path = run_config(config_path)
     compare_content(output_path, stats_path)
