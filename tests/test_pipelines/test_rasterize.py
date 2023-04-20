d2e7adc7cad1b01bb063e91fbf1f2fd94b02316e
Matic Lubej <matic.lubej@sinergise.com>
2023-04-20 12:38:46 +0200

    rename to more intuitive names

--- tests/test_pipelines/test_rasterize.py
+++ tests/test_pipelines/test_rasterize.py
@@ -13,9 +13,9 @@ def test_rasterize_file(config_and_stats_paths, experiment_name):
 
 
 @pytest.mark.chain
-@pytest.mark.order(before="test_rasterize_feature_resolution")
+@pytest.mark.order(before="test_rasterize_feature_with_resolution")
 @pytest.mark.parametrize("preparation_config,config", [("load_crops_vector_data", "rasterize_feature_shape")])
-def test_rasterize_feature_shape(config_and_stats_paths, preparation_config, config):
+def test_rasterize_feature_with_shape(config_and_stats_paths, preparation_config, config):
     preparation_config_path, _ = config_and_stats_paths("rasterize", preparation_config)
     config_path, stats_path = config_and_stats_paths("rasterize", config)
 
@@ -26,7 +26,7 @@ def test_rasterize_feature_shape(config_and_stats_paths, preparation_config, con
 
 @pytest.mark.chain
 @pytest.mark.parametrize("preparation_config,config", [("load_lulc_vector_data", "rasterize_feature_resolution")])
-def test_rasterize_feature_resolution(config_and_stats_paths, preparation_config, config):
+def test_rasterize_feature_with_resolution(config_and_stats_paths, preparation_config, config):
     preparation_config_path, _ = config_and_stats_paths("rasterize", preparation_config)
     config_path, stats_path = config_and_stats_paths("rasterize", config)
 
