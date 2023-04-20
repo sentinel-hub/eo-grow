b028ce7b5b16a25a38bbdbc319f600aeaa2296d7
Matic Lubej <matic.lubej@sinergise.com>
2023-04-20 12:25:12 +0200

    rename and restructure tests, update stats

--- tests/test_pipelines/test_rasterize.py
+++ tests/test_pipelines/test_rasterize.py
@@ -5,19 +5,31 @@ from eogrow.utils.testing import compare_content, run_config
 pytestmark = pytest.mark.integration
 
 
-@pytest.mark.parametrize("experiment_name", ["rasterize_pipeline_vector_file"])
-def test_rasterize_pipeline_vector_file(config_and_stats_paths, experiment_name):
+@pytest.mark.parametrize("experiment_name", ["rasterize_file"])
+def test_rasterize_file(config_and_stats_paths, experiment_name):
     config_path, stats_path = config_and_stats_paths("rasterize", experiment_name)
     output_path = run_config(config_path)
     compare_content(output_path, stats_path)
 
 
 @pytest.mark.chain
-@pytest.mark.parametrize("preparation_config, config", [("prepare_vector_data", "rasterize_pipeline_vector_feature")])
-def test_rasterize_pipeline_vector_feature(config_and_stats_paths, preparation_config, config):
+@pytest.mark.order(before="test_rasterize_feature_resolution")
+@pytest.mark.parametrize("preparation_config,config", [("load_crops_vector_data", "rasterize_feature_shape")])
+def test_rasterize_feature_shape(config_and_stats_paths, preparation_config, config):
     preparation_config_path, _ = config_and_stats_paths("rasterize", preparation_config)
     config_path, stats_path = config_and_stats_paths("rasterize", config)
 
     run_config(preparation_config_path)
+    output_path = run_config(config_path, reset_output_folder=False)
+    compare_content(output_path, stats_path)
+
+
+@pytest.mark.chain
+@pytest.mark.parametrize("preparation_config,config", [("load_lulc_vector_data", "rasterize_feature_resolution")])
+def test_rasterize_feature_resolution(config_and_stats_paths, preparation_config, config):
+    preparation_config_path, _ = config_and_stats_paths("rasterize", preparation_config)
+    config_path, stats_path = config_and_stats_paths("rasterize", config)
+
+    run_config(preparation_config_path, reset_output_folder=False)
     output_path = run_config(config_path, reset_output_folder=False)
     compare_content(output_path, stats_path)
