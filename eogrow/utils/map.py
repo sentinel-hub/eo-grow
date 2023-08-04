97cfe39f767669f356ba3f043db1b7fbba01b508
Žiga Lukšič <31988337+zigaLuksic@users.noreply.github.com>
2023-08-04 15:11:11 +0200

    regular precommit update (#260)

--- eogrow/utils/map.py
+++ eogrow/utils/map.py
@@ -136,10 +136,8 @@ def cogify(
 
     if version < "3.6.0" and resampling == "MODE":
         warnings.warn(
-            (
-                "GDAL versions below 3.6.0 have issues with `MODE` overview resampling. Trying to fix issue by setting"
-                " GDAL_OVR_CHUNK_MAX_SIZE to a large integer (2100000000)."
-            ),
+            "GDAL versions below 3.6.0 have issues with `MODE` overview resampling. Trying to fix issue by setting"
+            " GDAL_OVR_CHUNK_MAX_SIZE to a large integer (2100000000).",
             category=RuntimeWarning,
             stacklevel=2,
         )
