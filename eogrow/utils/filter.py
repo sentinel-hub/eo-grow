81ca2bd87a617d76625f60aa23ea8f45999b0738
Ziga Luksic <ziga.luksic@sinergise.com>
2023-08-18 13:43:07 +0200

    adjust filtration

--- eogrow/utils/filter.py
+++ eogrow/utils/filter.py
@@ -11,51 +11,37 @@ from fs.base import FS
 from tqdm.auto import tqdm
 
 from eolearn.core import FeatureType
-from eolearn.core.eodata_io import FilesystemDataInfo, get_filesystem_data_info
+from eolearn.core.eodata_io import get_filesystem_data_info
 
-from ..types import FeatureSpec, PatchList
+from ..types import PatchList
 
 
 def check_if_features_exist(
     filesystem: FS,
     eopatch_path: str,
-    features: Sequence[FeatureSpec],
+    features: Sequence[tuple[FeatureType, str]],
+    skip_bbox: bool = False,
+    skip_timestamps: bool = False,
 ) -> bool:
     """Checks whether an EOPatch in the given location has all specified features saved"""
     try:
         existing_data = get_filesystem_data_info(filesystem, eopatch_path, features)
-        meta_features = [spec for spec in features if isinstance(spec, FeatureType)]
-        regular_features = [spec for spec in features if isinstance(spec, tuple)]
-
-        if not all(_check_if_meta_feature_exists(ftype, existing_data) for ftype in meta_features):
-            return False
-
-        for ftype, fname in regular_features:
-            if ftype == FeatureType.META_INFO:
-                raise ValueError("Cannot check for a specific meta-info feature!")
-            if ftype not in existing_data.features or fname not in existing_data.features[ftype]:
-                return False
-        return True
-
     except (IOError, fs.errors.ResourceNotFound):
         return False
 
+    if not skip_bbox and existing_data.bbox is None:
+        return False
+    if not skip_timestamps and existing_data.timestamps is None:
+        return False
 
-def _check_if_meta_feature_exists(ftype: FeatureType, existing_data: FilesystemDataInfo) -> bool:
-    if ftype == FeatureType.BBOX and existing_data.bbox is None:
-        return False
-    if ftype == FeatureType.TIMESTAMPS and existing_data.timestamps is None:
-        return False
-    if ftype == FeatureType.META_INFO and existing_data.meta_info is None:
-        return False
-    return True
+    return all(fname in existing_data.features.get(ftype, []) for ftype, fname in features)
 
 
 def get_patches_with_missing_features(
     filesystem: FS,
     patches_folder: str,
     patch_list: PatchList,
-    features: Sequence[FeatureSpec],
+    features: Sequence[tuple[FeatureType, str]],
 ) -> PatchList:
     """Filters out names of those EOPatches that are missing some given features.
 
--- tests/utils/test_filter.py
+++ tests/utils/test_filter.py
@@ -1,5 +1,5 @@
 import datetime
-from itertools import chain, combinations, repeat
+from itertools import repeat
 
 import boto3
 import numpy as np
@@ -15,14 +15,14 @@ from eogrow.utils.filter import check_if_features_exist, get_patches_with_missin
 
 BUCKET_NAME = "mocked-test-bucket"
 PATCH_NAMES = [f"eopatch{i}" for i in range(5)]
-REAL_FEATURES = [FeatureType.BBOX, FeatureType.TIMESTAMPS, (FeatureType.DATA, "data"), (FeatureType.MASK, "mask")]
-MISSING_FEATURES = [FeatureType.META_INFO, (FeatureType.DATA, "no_data"), (FeatureType.MASK_TIMELESS, "mask")]
+MISSING = [(FeatureType.META_INFO, "beep"), (FeatureType.DATA, "no_data"), (FeatureType.MASK_TIMELESS, "mask")]
+EXISTING = [(FeatureType.DATA, "data"), (FeatureType.DATA, "data2"), (FeatureType.MASK, "mask")]
 
 
-def _prepare_fs(filesystem, eopatch):
+def _prepare_fs(filesystem, eopatch: EOPatch):
     """Saves the eopatch under the predefined names, where every second one only contains the BBOX and MASK"""
     for i, name in enumerate(PATCH_NAMES):
-        eopatch.save(name, filesystem=filesystem, features=[FeatureType.BBOX, FeatureType.MASK] if i % 2 else ...)
+        eopatch.save(name, filesystem=filesystem, features=... if i % 2 else [FeatureType.MASK], save_timestamps=i > 1)
 
 
 @pytest.fixture(name="eopatch", scope="session")
@@ -31,6 +31,7 @@ def eopatch_fixture():
     eopatch.timestamps = [datetime.datetime(2017, 1, 1, 10, 4, 7), datetime.datetime(2017, 1, 4, 10, 14, 5)]
     eopatch.mask["mask"] = np.zeros((2, 3, 3, 2), dtype=np.int16)
     eopatch.data["data"] = np.zeros((2, 3, 3, 2), dtype=np.int16)
+    eopatch.data["data2"] = np.zeros((2, 3, 3, 2), dtype=bool)
     eopatch.scalar["my scalar with spaces"] = np.array([[1, 2, 3], [1, 2, 3]])
     eopatch.scalar_timeless["my timeless scalar with spaces"] = np.array([1, 2, 3])
     return eopatch
@@ -55,21 +56,25 @@ def temp_fs_fixture(eopatch):
 
 @pytest.mark.parametrize(
     ("test_features", "expected_result"),
-    [(list(features), True) for features in chain(*(combinations(REAL_FEATURES, i) for i in range(4)))]
-    + [([missing], False) for missing in MISSING_FEATURES]
-    + [(REAL_FEATURES[:i] + [missing] + REAL_FEATURES[i:], False) for i, missing in enumerate(MISSING_FEATURES)],
+    [
+        ([EXISTING[0]], True),
+        (EXISTING[1:], True),
+        (EXISTING, True),
+        *(([missing], False) for missing in MISSING),
+        *((EXISTING[:i] + [missing] + EXISTING[i:], False) for i, missing in enumerate(MISSING)),
+    ],
 )
 def test_check_if_features_exist(mock_s3fs, temp_fs, test_features, expected_result):
     for filesystem in [mock_s3fs, temp_fs]:
-        assert check_if_features_exist(filesystem, PATCH_NAMES[0], test_features) == expected_result
+        # take the fourth patch because the first and third have missing features and the second has missing timestamps
+        assert check_if_features_exist(filesystem, PATCH_NAMES[3], test_features) == expected_result
 
 
 @pytest.mark.parametrize(
     ("features", "expected_num"),
     [
-        ([], 0),
-        ([FeatureType.BBOX], 0),
-        ([FeatureType.BBOX, (FeatureType.DATA, "data")], 2),
+        ([], 2),  # timestamps are missing
+        ([(FeatureType.DATA, "data")], 4),
         ([(FeatureType.DATA, "no_data"), (FeatureType.DATA, "data")], 5),
     ],
 )
