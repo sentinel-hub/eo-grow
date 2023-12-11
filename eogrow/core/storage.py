14b6a014ecc1b3ab1d9f39a1a118e4d490ba7308
Žiga Lukšič <31988337+zigaLuksic@users.noreply.github.com>
2023-12-11 08:46:56 +0100

    Better rounding in tests (#315)
    * fix shpy version issue
    
    * adjust rounding
    
    * make byoc tests work again
    
    * revert 'save_new_stats'

--- eogrow/core/storage.py
+++ eogrow/core/storage.py
@@ -7,7 +7,6 @@ from typing import Any, ClassVar, Dict, Literal, Optional
 import fs
 from pydantic import BaseSettings, Field
 
-import sentinelhub
 from eolearn.core.utils.fs import get_aws_credentials, get_filesystem, is_s3_path
 from sentinelhub import SHConfig
 
@@ -65,7 +64,7 @@ class StorageManager(EOGrowObject):
     def _prepare_sh_config(self) -> SHConfig:
         """Prepares an instance of `SHConfig` containing AWS credentials. In case given AWS profile doesn't exist it
         will show a warning and return a config without AWS credentials."""
-        sh_config = SHConfig(hide_credentials=True) if sentinelhub.__version__ < "3.9.0" else SHConfig()
+        sh_config = SHConfig()
 
         if self.is_on_s3() and self.config.aws_profile:
             sh_config = get_aws_credentials(aws_profile=self.config.aws_profile, config=sh_config)
