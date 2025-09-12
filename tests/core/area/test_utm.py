fa75ab65296eaa660ed80fe073242f03b5785a17
Matic Lubej <matic.lubej@planet.com>
2025-09-12 11:10:36 +0200

    update tests

--- tests/core/area/test_utm.py
+++ tests/core/area/test_utm.py
@@ -49,40 +49,53 @@ def test_cache_name(storage):
     assert area_manager.get_grid_cache_filename() == "UtmZoneAreaManager_test_area_2400_1100_120.0_55.0_0.0_0.0.gpkg"
 
 
-def test_create_utm_zone_grid():
-    area_geometry = Geometry(
+@pytest.mark.parametrize(
+    ("offset_x", "offset_y", "buffer_x", "buffer_y", "polygon_first", "polygon_last"),
+    [
+        (
+            0,
+            0,
+            0,
+            0,
+            Polygon([[710000, 5305000], [710000, 5310000], [715000, 5310000], [715000, 5305000], [710000, 5305000]]),
+            Polygon([[285000, 5325000], [285000, 5330000], [290000, 5330000], [290000, 5325000], [285000, 5325000]]),
+        ),
+        (
+            10,
+            20,
+            30,
+            40,
+            Polygon([[709980, 5304980], [709980, 5310060], [715040, 5310060], [715040, 5304980], [709980, 5304980]]),
+            Polygon([[284980, 5324980], [284980, 5330060], [290040, 5330060], [290040, 5324980], [284980, 5324980]]),
+        ),
+    ],
+)
+def test_create_utm_zone_grid(offset_x, offset_y, buffer_x, buffer_y, polygon_first, polygon_last):
+    # Covers UTM zones 33N and 34N
+    geometry = Geometry(
         "POLYGON ((17.87338 47.88549, 17.87338 48.06064, 18.14598 48.06064, 18.14598 47.88549, 17.87338 47.88549))",
         crs=CRS.WGS84,
-    )  # Covers UTM zones 33N and 34N
-
-    name_column = "eopatch_name"
-    bbox_size = (5000, 5000)
-    bbox_offset = (0, 0)
-    bbox_buffer = (0, 0)
-
-    grid = create_utm_zone_grid(area_geometry, name_column, bbox_size, bbox_offset, bbox_buffer)
+    )
+    grid = create_utm_zone_grid(geometry, "eopatch_name", (5000, 5000), (offset_x, offset_y), (buffer_x, buffer_y))
 
     assert isinstance(grid, dict)
     assert len(grid) == 2
     assert set(grid.keys()) == {CRS("32633"), CRS("32634")}
 
-    for gdf, n_expected_tiles in zip(grid.values(), (15, 15)):
+    n_expected_tiles = 15
+    for gdf in grid.values():
         assert isinstance(gdf, GeoDataFrame)
-        assert set(gdf.columns) == {"id", name_column, "geometry"}
+        assert set(gdf.columns) == {"id", "eopatch_name", "geometry"}
         assert len(gdf.index) == n_expected_tiles
 
     # explicit check for a few tiles
     assert grid[CRS("32633")].iloc[0].to_dict() == {
         "id": 0,
         "eopatch_name": "eopatch-id-00-col-0-row-0",
-        "geometry": Polygon(
-            [[710000, 5305000], [710000, 5310000], [715000, 5310000], [715000, 5305000], [710000, 5305000]]
-        ),
+        "geometry": polygon_first,
     }
     assert grid[CRS("32634")].iloc[-1].to_dict() == {
         "id": 29,
         "eopatch_name": "eopatch-id-29-col-2-row-4",
-        "geometry": Polygon(
-            [[285000, 5325000], [285000, 5330000], [290000, 5330000], [290000, 5325000], [285000, 5325000]]
-        ),
+        "geometry": polygon_last,
     }
--- tests/pipelines/test_batch_download.py
+++ tests/pipelines/test_batch_download.py
@@ -3,53 +3,35 @@ from shapely import Polygon
 
 from sentinelhub import CRS, Geometry
 
-from eogrow.pipelines.download_batch import create_batch_grid
+from eogrow.core.area.utm import create_utm_zone_grid
+from eogrow.pipelines.download_batch import to_batch_grid_format
 
 
-def test_create_batch_grid():
+def test_to_batch_grid_format():
+    # Covers UTM zones 33N and 34N
     area_geometry = Geometry(
         "POLYGON ((17.87338 47.88549, 17.87338 48.06064, 18.14598 48.06064, 18.14598 47.88549, 17.87338 47.88549))",
         crs=CRS.WGS84,
-    )  # Covers UTM zones 33N and 34N
+    )
 
     bbox_size = (5000, 5000)
-    bbox_offset = (10, 20)
-    bbox_buffer = (30, 40)
+    bbox_offset = (0, 0)
+    bbox_buffer = (0, 0)
     image_size = (512, 512)
     resolution = 10
 
-    grid = create_batch_grid(area_geometry, bbox_size, bbox_offset, bbox_buffer, image_size, resolution)
+    grid = create_utm_zone_grid(area_geometry, "identifier", bbox_size, bbox_offset, bbox_buffer)
+    grid = to_batch_grid_format(grid, image_size, resolution)
 
     assert isinstance(grid, dict)
     assert len(grid) == 2
     assert set(grid.keys()) == {CRS("32633"), CRS("32634")}
 
-    for gdf, n_expected_tiles in zip(grid.values(), (15, 15)):
+    n_expected_tiles = 15
+    for gdf in grid.values():
         assert isinstance(gdf, GeoDataFrame)
         assert set(gdf.columns) == {"id", "identifier", "geometry", "width", "height", "resolution"}
         assert len(gdf.index) == n_expected_tiles
         assert all(gdf["width"] == image_size[0])
         assert all(gdf["height"] == image_size[1])
         assert all(gdf["resolution"] == resolution)
-
-    # explicit check for a few tiles
-    assert grid[CRS("32633")].iloc[0].to_dict() == {
-        "id": 0,
-        "identifier": "eopatch-id-00-col-0-row-0",
-        "height": 512,
-        "resolution": 10,
-        "width": 512,
-        "geometry": Polygon(
-            [[709980, 5304980], [709980, 5310060], [715040, 5310060], [715040, 5304980], [709980, 5304980]]
-        ),
-    }
-    assert grid[CRS("32634")].iloc[-1].to_dict() == {
-        "id": 29,
-        "identifier": "eopatch-id-29-col-2-row-4",
-        "height": 512,
-        "resolution": 10,
-        "width": 512,
-        "geometry": Polygon(
-            [[284980, 5324980], [284980, 5330060], [290040, 5330060], [290040, 5324980], [284980, 5324980]]
-        ),
-    }
